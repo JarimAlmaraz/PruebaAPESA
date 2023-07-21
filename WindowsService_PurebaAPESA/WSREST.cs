b12660b6b53e16f1f2904bfe0d35707c730c0fcb
JarimAlmaraz <101910586+JarimAlmaraz@users.noreply.github.com>
2023-07-21 16:09:54 -0600

    Add files via upload

---
+++ WindowsService_PurebaAPESA/WSREST.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WindowsService_PurebaAPESA
+{
+    [ServiceContract]
+    public class MiServicioREST
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, UriTemplate = "Sumar")]
+        public int SumarNumeros(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
